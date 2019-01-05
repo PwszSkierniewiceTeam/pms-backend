deb0c0b975a1dacce1ea4065448f9e27f0415cce
Pawel Kosmatka <pawel.kosmatka@outlook.com>
2019-01-05 14:12:29 +0100

    new db schema

--- database-schema/pms_all_tables_dev_only.sql
+++ database-schema/pms_all_tables_dev_only.sql
@@ -31,27 +31,17 @@ CREATE INDEX IX_projectId ON pms.UsersProjects(projectId);
 ALTER TABLE pms.UsersProjects ADD CONSTRAINT FK_UsersProjects_Users FOREIGN KEY (userId) REFERENCES pms.Users(id);
 ALTER TABLE pms.UsersProjects ADD CONSTRAINT FK_UsersProjects_Projects FOREIGN KEY (projectId) REFERENCES pms.Projects(id);
 
-
 CREATE TABLE pms.Tasks (
     id varchar(36) DEFAULT uuid() NOT NULL,
     name varchar(30) NOT NULL,
     projectId varchar(36) NOT NULL,
     description varchar(255) NOT NULL,
     type varchar(30) NOT NULL,
+    status varchar(30) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (projectId) REFERENCES Projects(id)
+    FOREIGN KEY (userId) REFERENCES Users(id)
 );
 CREATE UNIQUE INDEX UIX_taskId ON pms.Tasks(id);
 CREATE INDEX IX_projectId ON pms.Tasks(projectId);
-
-CREATE TABLE pms.UsersTasks (
-    taskId varchar(36) NOT NULL,
-    userId varchar(36) NOT NULL,
-    role varchar(20) NOT NULL,
-    CONSTRAINT PK_UsersTasks PRIMARY KEY (userId, taskId)
-);
-CREATE INDEX IX_userId ON pms.UsersTasks(userId);
-CREATE INDEX IX_taskId ON pms.UsersTasks(taskId);
-
-ALTER TABLE pms.UsersTasks ADD CONSTRAINT FK_UsersTasks_Users FOREIGN KEY (userId) REFERENCES pms.Users(id);
-ALTER TABLE pms.UsersTasks ADD CONSTRAINT FK_UsersTasks_Projects FOREIGN KEY (taskId) REFERENCES pms.Tasks(id);
+CREATE INDEX IX_userId ON pms.Tasks(userId);
